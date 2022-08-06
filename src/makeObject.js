702254dfacb4661a9225c4a816b22b6dd5fdb063
fatemeh jahangiri <jahangiri.ir.it@gmail.com>
2022-08-07 00:04:52 +0430

    Feature: Convert array to object based on depth

--- src/makeObject.js
+++ src/makeObject.js
@@ -1,5 +1,8 @@
-function makeObject() {
-    // ...your code
+function makeObject(input, depth) {
+    // @ts-ignore
+    return input.reduce((o, [k, v]) => Object.assign(o, {
+        [k]: (Array.isArray(v) && depth > 0) ? makeObject(v, depth - 1) : v,
+    }), {});
 }
 
 // do NOT remove the following line
